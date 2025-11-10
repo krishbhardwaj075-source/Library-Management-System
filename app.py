be8144371b45e0d674091b1ec16193fabd1ec3d9
krishbhardwaj075-source <your_github_email@example.com>
2025-11-11 00:23:18 +0530

    new changes

--- app.py
+++ app.py
@@ -108,5 +108,8 @@ def return_book(issue_id):
         db.session.commit()
     return redirect(url_for('index'))
 # ---------------------- Run Server ----------------------
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
